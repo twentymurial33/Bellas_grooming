d2c8766392119cc50a18ea8ec539048fb0659ed4
Murial Anindo <murial.anindo@lonett.com>
2023-10-31 12:30:04 -0400

    added the component and pages

---
+++ my-app/src/components/Landing.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <div>
+        <nav> 
+          <h1>Bellas Grooming Salon</h1>
+          <p>The Petcare MarketPlace</p>
+        <ul>
+            <li>Salon</li>
+            <li>Mobile</li>
+            <li>Pricing</li>
+        </ul>
+        </nav>
+       <button>Book Now </button>      
+    </div>
+            
+  )
+}
+
+export default Landing
